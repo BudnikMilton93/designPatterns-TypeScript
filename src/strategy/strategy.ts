02e31ff8ff0c5434db6c7951b1e98d11faf663ab
BudnikMilton93 <budnikmilton93@gmail.com>
2025-12-27 20:10:05 -0300

    Patrón Strategy listo.

--- src/strategy/strategy.ts
+++ src/strategy/strategy.ts
@@ -1,3 +1,43 @@
+/**
+ * STRATEGY PATTERN
+ *
+ * Cuándo usar este patrón:
+ * - Cuando tenés múltiples formas de realizar una misma acción
+ *   (por ejemplo: pagos, cálculos, validaciones).
+ * - Cuando querés evitar grandes bloques de if / else o switch.
+ * - Cuando necesitás cambiar el comportamiento en tiempo de ejecución.
+ *
+ * Ejemplos reales:
+ * - Métodos de pago (tarjeta, PayPal, transferencia)
+ * - Algoritmos de descuento
+ * - Estrategias de autenticación
+ */
+
 interface PaymentStrategy {
     pay(amount: number): void;
 }
+
+class CreditCardPayment implements PaymentStrategy {
+    pay(amount: number): void {
+        console.log(`Paid ${amount} with Credit Card`);
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+    pay(amount: number): void {
+        console.log(`Paid ${amount} with PayPal`);
+    }
+}
+
+class Checkout {
+    constructor(private paymentStrategy: PaymentStrategy) { }
+
+    process(amount: number): void {
+        this.paymentStrategy.pay(amount);
+    }
+}
+
+// Demotración
+
+const checkout = new Checkout(new PayPalPayment());
+checkout.process(100);
